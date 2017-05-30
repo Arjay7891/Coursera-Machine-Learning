bb41eaea3b94517ac8e6bfaccda962ba5a239d00
Roman Hennig <rjhennig26@gmail.com>
2017-05-30 17:06:30 -0400

    add ex5 solutions

--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -15,6 +15,7 @@ lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
 % You need to return these variables correctly.
 error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
+error_test(i) = zeros(length(lambda_vec), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return training errors in 
@@ -39,7 +40,14 @@ error_val = zeros(length(lambda_vec), 1);
 %
 %
 
-
+for i = 1:length(lambda_vec)
+  lambda = lambda_vec(i);
+  theta = trainLinearReg(X, y, lambda);
+  error_train(i) = linearRegCostFunction(X, y, ...
+    theta, 0);
+  error_val(i) = linearRegCostFunction(Xval, yval, ...
+    theta, 0);
+end
 
 
 
