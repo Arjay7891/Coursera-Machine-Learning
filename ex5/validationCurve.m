015df2f765bd5dbc8fa1f84ef602a20598440236
Roman Hennig <romanhennig@Romans-MacBook-Pro.local>
2017-06-01 23:18:58 -0400

    add ex6 and lectures

--- ex5/validationCurve.m
+++ ex5/validationCurve.m
@@ -15,7 +15,6 @@ lambda_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10]';
 % You need to return these variables correctly.
 error_train = zeros(length(lambda_vec), 1);
 error_val = zeros(length(lambda_vec), 1);
-error_test(i) = zeros(length(lambda_vec), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return training errors in 
@@ -48,12 +47,6 @@ for i = 1:length(lambda_vec)
   error_val(i) = linearRegCostFunction(Xval, yval, ...
     theta, 0);
 end
-
-
-
-
-
-
 
 
 % =========================================================================
